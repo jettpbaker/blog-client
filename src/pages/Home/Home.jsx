78fdfe1c6e87a587834147833c23e778123eb791
jettpbaker <jett.p.baker@gmail.com>
2025-04-08 16:41:16 +1000

    home page now renders posts in a list

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,27 +1,11 @@
-import { Link } from 'react-router'
-import { useMemo, useState } from 'react'
-import useFetch from '../../hooks/useFetch'
+import { useState } from 'react'
 import { PostContainer } from './PostContainer'
 import { CursorEffect } from '../../components/CursorEffect/CursorEffect'
 import styles from './Home.module.css'
 import { useEffect } from 'react'
-const API_URL = import.meta.env.VITE_API_URL
 
 function Home() {
   const [cursorPosition, setCursorPosition] = useState(0, 0)
-
-  const url = `${API_URL}/posts`
-  const options = useMemo(
-    () => ({
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }),
-    []
-  )
-
-  const { loading, error } = useFetch(url, options)
 
   useEffect(() => {
     const handlePointerMove = (e) => {
@@ -38,18 +22,13 @@ function Home() {
     }
   }, [])
 
-  if (loading) {
-    return <div>Loading...</div>
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>
-  }
-
   return (
     <>
       <main className={styles.homeContainer}>
-        <PostContainer />
+        <div className={styles.homeContentContainer}>
+          <h1>Posts</h1>
+          <PostContainer />
+        </div>
         <div className={styles.blurOverlay}></div>
         <CursorEffect cursorPosition={cursorPosition} />
       </main>
