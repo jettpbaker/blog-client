3fccf57c237c60e2ed512f163c9a93c5f27ae79a
jettpbaker <jett.p.baker@gmail.com>
2025-04-10 19:24:25 +1000

    added more logs for debugging

--- src/components/PostPublishModal/PostPublishModal.jsx
+++ src/components/PostPublishModal/PostPublishModal.jsx
@@ -73,6 +73,7 @@ export function PostPublishModal({ handleModalClose, postMarkdown }) {
 
       try {
         console.log('Trying to create a new post...')
+        console.log(url)
         postNewPost(url, options)
         cacheDelete(cacheKey)
       } catch (err) {
